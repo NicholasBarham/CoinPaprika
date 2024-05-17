89b186fdf548d0b706536c8ea87133be52edf7bc
Nicholas Barham <n-barham@live.co.uk>
2024-05-17 16:19:10 +0100

    Refactored CoinManger functions to return flows

--- app/src/main/java/com/example/coinpaprika/data/api/CoinManager.kt
+++ app/src/main/java/com/example/coinpaprika/data/api/CoinManager.kt
@@ -4,9 +4,10 @@ import com.example.coinpaprika.data.api.api_data.ApiError
 import com.example.coinpaprika.data.api.api_data.Coin
 import com.example.coinpaprika.data.api.api_data.CoinDetail
 import com.example.coinpaprika.data.api.api_data.Result
+import kotlinx.coroutines.flow.Flow
 
 interface CoinManager {
-    suspend fun getAllCoins(): Result<List<Coin>, ApiError>
+    suspend fun getAllCoins(): Flow<Result<List<Coin>, ApiError>>
 
-    suspend fun getCoinById(id: String): Result<CoinDetail, ApiError>
+    suspend fun getCoinById(id: String): Flow<Result<CoinDetail, ApiError>>
 }
--- app/src/main/java/com/example/coinpaprika/data/api/CoinManagerImpl.kt
+++ app/src/main/java/com/example/coinpaprika/data/api/CoinManagerImpl.kt
@@ -7,6 +7,8 @@ import com.example.coinpaprika.data.api.api_data.CoinDetailDto
 import com.example.coinpaprika.data.api.api_data.CoinDto
 import com.example.coinpaprika.data.api.api_data.Result
 import com.example.coinpaprika.data.api.api_mappers.Mapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import java.net.UnknownHostException
 import javax.inject.Inject
 
@@ -15,46 +17,49 @@ class CoinManagerImpl @Inject constructor(
     private val coinMapper: Mapper<Coin, CoinDto>,
     private val coinDetailMapper: Mapper<CoinDetail, CoinDetailDto>
 ) : CoinManager {
-    override suspend fun getAllCoins(): Result<List<Coin>, ApiError> {
-        return try {
-            val response = coinApi.getAllCoins()
-            if (response.isSuccessful) {
-                response.body()?.let { coinDtoList ->
-                    val coins = coinDtoList.map { coinMapper.toDomain(it) }
-                    Result.Success(coins)
-                } ?: Result.Error(ApiError.RETURNING_NULL)
-            } else {
-                val apiError = getApiErrorFromCode(response.code())
-                Result.Error(apiError)
+    override suspend fun getAllCoins(): Flow<Result<List<Coin>, ApiError>> {
+        return flow {
+            try {
+                val response = coinApi.getAllCoins()
+                if (response.isSuccessful) {
+                    response.body()?.let { coinDtoList ->
+                        val coins = coinDtoList.map { coinMapper.toDomain(it) }
+                        emit(Result.Success(coins))
+                    } ?: emit(Result.Error(ApiError.RETURNING_NULL))
+                } else {
+                    val apiError = getApiErrorFromCode(response.code())
+                    emit(Result.Error(apiError))
+                }
+            } catch (e: UnknownHostException) {
+                println("ERROR: $e")
+                emit(Result.Error(ApiError.NO_INTERNET))
+            } catch (e: Exception) {
+                println("ERROR: $e")
+                emit(Result.Error(ApiError.UNKNOWN))
             }
-        } catch (e: UnknownHostException) {
-            println("ERROR: $e")
-            Result.Error(ApiError.NO_INTERNET)
-        } catch (e: Exception) {
-            println("ERROR: $e")
-            Result.Error(ApiError.UNKNOWN)
         }
     }
 
-    override suspend fun getCoinById(id: String): Result<CoinDetail, ApiError> {
-        return try {
-            val response = coinApi.getCoin(id)
-
-            if (response.isSuccessful) {
-                response.body()?.let { detailDto ->
-                    val detail = coinDetailMapper.toDomain(detailDto)
-                    return Result.Success(detail)
-                } ?: return Result.Error(ApiError.RETURNING_NULL)
-            } else {
-                val apiError = getApiErrorFromCode(response.code())
-                return Result.Error(apiError)
+    override suspend fun getCoinById(id: String): Flow<Result<CoinDetail, ApiError>> {
+        return flow {
+            try {
+                val response = coinApi.getCoin(id)
+                if (response.isSuccessful) {
+                    response.body()?.let { detailDto ->
+                        val detail = coinDetailMapper.toDomain(detailDto)
+                        emit(Result.Success(detail))
+                    } ?: emit(Result.Error(ApiError.RETURNING_NULL))
+                } else {
+                    val apiError = getApiErrorFromCode(response.code())
+                    emit(Result.Error(apiError))
+                }
+            } catch (e: UnknownHostException) {
+                println("ERROR: $e")
+                emit(Result.Error(ApiError.NO_INTERNET))
+            } catch (e: Exception) {
+                println("ERROR: $e")
+                emit(Result.Error(ApiError.UNKNOWN))
             }
-        } catch (e: UnknownHostException) {
-            println("ERROR: $e")
-            Result.Error(ApiError.NO_INTERNET)
-        } catch (e: Exception) {
-            println("ERROR: $e")
-            Result.Error(ApiError.UNKNOWN)
         }
     }
 
